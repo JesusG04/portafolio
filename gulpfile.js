9e5c8ade50746374551a27b4a60ee63cd89c5153
JesusG04 <126627917+JesusG04@users.noreply.github.com>
2025-02-01 00:17:45 -0600

    Agregando el js con la funcionalidad de la navegacion y los proyectos reales

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, watch, series } = require('gulp');
+
+const terser = require('gulp-terser');
+const plumber = require('gulp-plumber');
+
+
+const paths = {
+    js: 'src/**/*.js',
+};
+
+
+function js(done) {
+    src(paths.js)
+        .pipe(plumber())
+        .pipe(terser())
+        .pipe(dest('./docs/src'));
+    done();
+}
+
+function dev() {
+    watch(paths.js, js);
+}
+
+module.exports = {
+    js,
+    dev,
+    default: series(js, dev),
+};
